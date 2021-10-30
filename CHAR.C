76dfdcef9ba588c50a0829b5479062cd4b546e37
pravenkumarR <93313725+pravenkumarR@users.noreply.github.com>
2021-10-30 15:14:26 +0530

    Add files via upload

---
+++ CHAR.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   char val1,val2;
+   scanf("%c",&val1);
+   printf("%x\n",val1);
+   getch();
+  /* scanf("%c",&val2);
+   printf("%c\n",val2);
+   getch();*/
+}
+
+
---
+++ POORIRSK.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int poori,ragul,sanjay,kamali,r_poori,s_poori,k_poori;
+   clrscr();
+   printf("Enter total poori:");
+   scanf("%d",&poori);
+   printf("Enter ragul's poori:");
+   scanf("%d",&ragul);
+   printf("Enter sanjay's poori:");
+   scanf("%d",&sanjay);
+   printf("Enter kamali's poori:");
+   scanf("%d",&kamali);
+   r_poori=poori*ragul/100;
+   s_poori=poori*sanjay/100;
+   k_poori=poori*kamali/100;
+   poori=poori-r_poori-s_poori
+   printf("r and s's poori:%d\n",poori);
+   poori=poori-k_poori;
+   printf("Balance poori:%d",poori);
+   getch();
+}
+
+
+
+
+
